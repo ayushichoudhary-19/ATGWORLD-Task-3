8c3856589e0325998484183742b98fb5c65b1b6b
Ayushi Choudhary <73214455+ayushichoudhary-19@users.noreply.github.com>
2024-01-23 12:29:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-function simulateLoading() {
-    const submitButton = document.querySelector('.submit-button');
-    submitButton.disabled = true;
-  
-    setTimeout(() => {
-      submitButton.disabled = false;
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector(".submit-button");
+  const input = document.querySelector(".input");
+  const loadingIcon = document.querySelector(".loading-icon");
+  let actionCompleted = false;
+
+  button.addEventListener("click", handleButtonClick);
+
+  function handleButtonClick() {
+    if (actionCompleted) {
+      return;
+    }
+
+    loadingIcon.style.display = "inline";
+    button.disabled = true;
+    input.disabled = true;
+
+    setTimeout(function () {
+      loadingIcon.style.display = "none"; 
+      button.disabled = false;
+      input.disabled = false;
+      actionCompleted = true;
+      input.classList.add("input-success");
+      button.classList.add("button-success");
     }, 2000);
   }
-  
+});
